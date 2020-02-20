d4ddcd8302fea0a63eda10d1914f47e02fa06980
royrliu <royrliu@qq.com>
2020-02-20 09:56:50 +0800

    add config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   server: {
     port: 3100, // default: 3000
-    // host: '0.0.0.0' // default: localhost
+    host: '0.0.0.0' // default: localhost
   },
   /*
   ** Headers of the page
